7c109bd860a3646ecfc08c62620b214b8bb0c4dc
Juan Kaenel <kaenel10@gmail.com>
2020-11-16 12:14:44 -0300

    Iniciando proyecto

---
+++ src/components/Card.vue
@@ -0,0 +1,19 @@
+<template>
+  <div class="col-12 col-sm-4 col-md-3 col-lg-2 mb-3 mb-3">
+    <div class="card">
+        <img src="" class="card-img-top" alt="" />
+        <div class="card-body">
+            <h5 class="card-title"></h5>
+            <p class="card-text">$ </p>
+            <button class="btn btn-dark">Comprar</button>
+        </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {};
+</script>
+
+<style>
+</style>
--- src/store/index.js
+++ src/store/index.js
@@ -2,10 +2,26 @@ import { createStore } from 'vuex'
 
 export default createStore({
   state: {
+    productos: []
   },
   mutations: {
+    // Modifican el state
+    SET_PRODUCTO(state, payload){
+      state.productos = payload;
+      console.log(state.productos);
+    }
   },
   actions: {
+    async fetchData({commit}){
+      try {
+        const res = await fetch('api.json')
+        const data = await res.json()
+        // Ejecuto commit para llamar a la mutación
+        commit('SET_PRODUCTO',data)
+      } catch (error) {
+        console.log(error);
+      }
+    }
   },
   modules: {
   }
